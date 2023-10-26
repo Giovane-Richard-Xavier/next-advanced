4cfbb687992c5232111badf535089e3750a71efa
Giovane-Richard-Xavier <giovane@rlvtecnologia.com.br>
2023-10-26 18:25:45 -0300

    feat/inial pwa configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+const isProd = process.env.NODE_ENV === "production";
+const withBundlerAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALIZE === "true",
+});
 const nextConfig = {
   reactStrictMode: true,
-}
+  pwa: { dest: "public", disable: !isProd },
+};
 
-module.exports = nextConfig
+module.exports = withBundlerAnalyzer(withPWA(nextConfig));
+// module.exports = nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="manifest" href="manifes.json" />
+        <meta name="description" content="painel Admin" />
+        <meta name="theme-color" content="#322659" />
+        <link rel="apple-touch-icon" href="images/icon-rlv.png" />
+      </Head>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
